89d62a2b6575bb0d8b41d02a64a93e073574c2a3
Arvind Purohit <purarvin@amazon.com>
2021-10-25 13:21:05 -0700

    first commit

---
+++ src/main/java/easy/array/TwoSum.java
@@ -0,0 +1,9 @@
+package easy.array;
+
+public class TwoSum {
+
+    public boolean twoSum(int[] arr, int sum)
+    {
+        return true;
+    }
+}
---
+++ src/test/java/easy/array/TwoSumTest.java
@@ -0,0 +1,15 @@
+package easy.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    @Test
+    public void twoSumTest()
+    {
+        Assertions.assertEquals(true, true);
+    }
+}
