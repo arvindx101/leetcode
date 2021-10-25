551207c12b90b51a57ac172677c4e756e4d39c0d
Arvind Purohit <purarvin@amazon.com>
2021-10-25 14:51:07 -0700

    Implemented twoSum in Java

--- src/main/java/easy/array/TwoSum.java
+++ src/main/java/easy/array/TwoSum.java
@@ -1,9 +1,25 @@
 package easy.array;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class TwoSum {
 
-    public boolean twoSum(int[] arr, int sum)
-    {
-        return true;
+    public boolean twoSum(int[] nums, int targetSum) {
+        boolean result = false;
+        if (nums != null && nums.length >= 1) {
+            Set<Integer> set = new HashSet<>(nums.length);
+            for (int num : nums) {
+                if (set.contains(targetSum - num)) {
+                    result = true;
+                    break;
+                }
+                set.add(num);
+            }
+        }
+
+        return result;
     }
 }
